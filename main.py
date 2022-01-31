484f073dfe8eb195ca10c8add132eb994f878de6
Rocco-OCSB <97174100+Rocco-OCSB@users.noreply.github.com>
2022-01-31 14:29:28 +0000

    stuff

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-# NAME OF AUTHOR:  
-# NAME OF THE PROGRAM:  
-# DATE OF CREATION:  
-# PURPOSE OF PROGRAM:  
+# NAME OF AUTHOR: Rocco Crupi
+# NAME OF THE PROGRAM: Snake game 
+
+import pygame
+import random
+import time
+from pygame import Color, draw, display, time
+
+pygame.init()
+
+dis_width = 600
+dis_height = 400
+
+clock = pygame.time.Clock()
+
+dis = pygame.display.set_mode((dis_width, dis_height))
+pygame.display.set_caption('Snake Game by Rocco')
+
+
+
+
 
 # VARIABLE DEFINITION
 
