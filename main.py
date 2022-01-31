6456e600aaf14971339dd6f47fbb8fd71d2c0f19
Rocco-OCSB <97174100+Rocco-OCSB@users.noreply.github.com>
2022-01-31 15:20:27 +0000

    stuff

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from pygame import Color, draw, display, time
 
 pygame.init()
 
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+
 dis_width = 600
 dis_height = 400
 
@@ -16,14 +19,26 @@ clock = pygame.time.Clock()
 dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('Snake Game by Rocco')
 
-
-
-
-
-# VARIABLE DEFINITION
-
-# INPUT
-
-# PROCESSING
-
-# OUTPUT
+snake_block = 10
+snake_speed = 10
+ 
+font_style = pygame.font.SysFont("bahnschrift", 25)
+score_font = pygame.font.SysFont("comicsansms", 35)
+ 
+ 
+def score(score):
+    value = score_font.render("Your Score: " + str(score), True, yellow)
+    dis.blit(value, [0, 0])
+ 
+ 
+ 
+def theSnake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
+ 
+ 
+def message(msg, color):
+    mesg = font_style.render(msg, True, color)
+    dis.blit(mesg, [dis_width / 6, dis_height / 3])
+ 
+  
